3e8f70bfe6455c49052ed702f2368234e2751174
Srini Motheram <srini@srinimoeramsmbp.wowway.com>
2017-03-06 20:48:37 -0500

    final working

--- LandMarks/ViewController.swift
+++ LandMarks/ViewController.swift
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
         let pa1 = MKPointAnnotation()
         pa1.coordinate = CLLocationCoordinate2D(latitude: lm.lat, longitude: lm.long)
         pa1.title = lm.landMarkItemName + " " + lm.city
-        pa1.subtitle = lm.state + " " + lm.zipCode + " " + lm.desc
+        pa1.subtitle = lm.streetAddress + " " + lm.state + " " + lm.zipCode + " " + lm.desc
         
         
         landMarksMap.addAnnotations([pa1])
@@ -60,12 +60,24 @@ class ViewController: UIViewController {
     
     func fillLandMarksArray() -> Void{
         
-        let landMark1 = LandMarkItem(landMarkItemName: "test", streetAddress: "test", city: "Detroit", state: "MI", zipCode: "48187", lat: 42, long: -83, desc: "landmark1")
+        let landMark1 = LandMarkItem(landMarkItemName: "WSU", streetAddress: "west warren ave", city: "Detroit", state: "MI", zipCode: "48202", lat: 42.3520112, long: -83.0854075, desc: "Wayne State University")
         
-        let landMark2 = LandMarkItem(landMarkItemName: "test2", streetAddress: "test2", city: "Detroit", state: "MI", zipCode: "48187", lat: 42.123, long: -83.123, desc: "landmark2")
+        let landMark2 = LandMarkItem(landMarkItemName: "Fox Theater", streetAddress: "2211 woodward ave", city: "Detroit", state: "MI", zipCode: "48201", lat: 42.3383693, long: -83.0550822, desc: "Fox Theater")
         
-         let landMark3 = LandMarkItem(landMarkItemName: "test3", streetAddress: "test3", city: "Detroit", state: "MI", zipCode: "48187", lat: 42.234, long: -83.234, desc: "landmark3")
-        landMarksArray.append(contentsOf: [landMark1, landMark2, landMark3])
+         let landMark3 = LandMarkItem(landMarkItemName: "Cobo Center", streetAddress: "1 washington blvd", city: "Detroit", state: "MI", zipCode: "48226", lat: 42.3266199, long: -83.0521407, desc: "Cobo Center")
+        let landMark4 = LandMarkItem(landMarkItemName: "Joe Louis", streetAddress: "19 Steve Yzerman Dr", city: "Detroit", state: "MI", zipCode: "48226", lat: 42.3252134, long: -83.0535564, desc: "Joe Louis")
+        let landMark5 = LandMarkItem(landMarkItemName: "Detroit Institute of Arts", streetAddress: "5200 woodward ave", city: "Detroit", state: "MI", zipCode: "48202", lat: 42.3594199, long: -83.0667651, desc: "Detroit Institute of Arts")
+        
+        let landMark6 = LandMarkItem(landMarkItemName: "Detroit Public Library", streetAddress: "5100 woodward ave", city: "Detroit", state: "MI", zipCode: "48202", lat: 42.3585883, long: -83.0689554, desc: "Detroit Institute of Arts")
+        
+        let landMark7 = LandMarkItem(landMarkItemName: "ISKCON", streetAddress: "383 Lenox", city: "Detroit", state: "MI", zipCode: "48215", lat: 42.3614399, long: -82.9483467, desc: "ISKCON")
+        
+         let landMark8 = LandMarkItem(landMarkItemName: "Eastern Market", streetAddress: "2900 Riopelle Street", city: "Detroit", state: "MI", zipCode: "48207", lat: 42.3495025, long: -83.0494247, desc: "Eastern Market")
+        
+        let landMark9 = LandMarkItem(landMarkItemName: "GM Ren Cen", streetAddress: "100 Renaissance Center", city: "Detroit", state: "MI", zipCode: "48243", lat: 42.3293284, long: -83.0397632, desc: "GM Ren Cen")
+        
+        let landMark10 = LandMarkItem(landMarkItemName: "Comerica Park", streetAddress: "2100 Woodward ave", city: "Detroit", state: "MI", zipCode: "48201", lat: 42.3371069, long: -83.0522998, desc: "Comerica Park")
+        landMarksArray.append(contentsOf: [landMark1, landMark2, landMark3, landMark4, landMark5, landMark6, landMark7, landMark8, landMark9, landMark10])
         
         //return [landMark1,landMark2, landMark3]
     }
